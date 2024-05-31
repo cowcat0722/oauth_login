75f95f4a253ce77eb9bbf85ef957280e51bb3e88
Pakchan <1625787@donga.ac.kr>
2024-05-31 11:25:42 +0900

    User 작성

--- src/main/java/com/example/loginapp/user/UserRepository.java
+++ src/main/java/com/example/loginapp/user/UserRepository.java
@@ -1,5 +1,8 @@
 package com.example.loginapp.user;
 
-public class UserRepository
-{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(@Param("username") String username);
 }
