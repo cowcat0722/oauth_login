fcdbc5e1ed965847e94ba0365c57045a4b2b17d9
Pakchan <1625787@donga.ac.kr>
2024-05-31 11:36:31 +0900

    Shop 작성, 머스테치 적용

--- src/main/java/com/example/loginapp/shop/ShopRepository.java
+++ src/main/java/com/example/loginapp/shop/ShopRepository.java
@@ -1,4 +1,7 @@
 package com.example.loginapp.shop;
 
-public class ShopRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopRepository extends JpaRepository<Shop, Integer> { // <Entity 이름, Id 타입>
+
 }
